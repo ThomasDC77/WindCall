f9f74394c8c5d76c39f3b25f5e7c58e76e21a1f1
Thomas da Cunha <thomas.dacunha77@gmail.com>
2022-12-05 12:00:56 +0100

    formulaires et route de l'alerte créée

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_094637) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_090054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_094637) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "alerts", force: :cascade do |t|
+    t.string "telephone_number"
+    t.string "difficulty"
+    t.integer "wind_min"
+    t.integer "wind_max"
+    t.datetime "time"
+    t.string "address"
+    t.integer "perimeter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "spots", force: :cascade do |t|
