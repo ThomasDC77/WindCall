bcfb5133c574ab1cac2a402fa75a96e364cea01c
Florian Calabrese florian.calabrese@gmail.com <florian.calabrese@gmail.com>
2022-12-06 10:34:13 +0100

    travail sur le front

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -16,7 +16,7 @@ class SpotsController < ApplicationController
 
   def show
     @spot = Spot.find(params[:id])
-    @weathers = @spot.weathers.where(id: params[:weather_ids].split(" "))
+    @weathers = @spot.weathers.where(id: params[:weather_ids].split(" ")) if params[:weather_ids].present?
   end
 
   private
