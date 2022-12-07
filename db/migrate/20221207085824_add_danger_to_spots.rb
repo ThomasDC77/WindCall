37a728cf60446d97c045676aac8b1be2578d5e60
Thomas da Cunha <thomas.dacunha77@gmail.com>
2022-12-07 13:47:20 +0100

    map with arrow who follow the wind direction done

---
+++ db/migrate/20221207085824_add_danger_to_spots.rb
@@ -0,0 +1,5 @@
+class AddDangerToSpots < ActiveRecord::Migration[7.0]
+  def change
+    add_column :spots, :danger, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_104901) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_085824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_104901) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "criteria"
+    t.text "technique"
+    t.text "danger"
   end
 
   create_table "weathers", force: :cascade do |t|
